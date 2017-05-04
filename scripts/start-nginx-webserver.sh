90bcac6ad38567962555f47c6cc81e36a153e717
chenzhihui <chenzhihui244@msn.com>
2017-05-04 09:55:12 +0800

    add start-nginx-webserver.sh

---
+++ scripts/start-nginx-webserver.sh
@@ -0,0 +1,26 @@
+
+#!/bin/sh
+
+TOPDIR=`pwd`
+
+source scripts/profile
+
+setup_webserver()
+{
+	cp conf/nginx/nginx.conf.webserver $NGINX_PATH/conf/nginx.conf
+}
+
+setup_webserver
+
+# start nginx
+$NGINX_PATH/sbin/nginx -s stop
+sleep 1
+$NGINX_PATH/sbin/nginx
+sleep 1
+ps -ef | grep nginx | grep -v grep | grep -v "start-nginx" | wc -l
+
+# stop nginx
+#$NGINX_PATH/sbin/nginx -s stop
+
+# reload nginx
+#$NGINX_PATH/sbin/nginx -s reload
