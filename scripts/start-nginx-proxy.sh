f943bfd50a7d5ffba482eedb17abab6b0d606499
chenzhihui <chenzhihui244@msn.com>
2017-05-05 11:33:51 +0800

    seperate proxy start

--- scripts/start-nginx-proxy.sh
+++ scripts/start-nginx-proxy.sh
@@ -1,15 +1,11 @@
+
 #!/bin/sh
 
 TOPDIR=`pwd`
 
-backend_server="192.168.1.105:80"
+backend_server="192.168.1.199:80"
 
 source scripts/profile
-
-setup_webserver()
-{
-	cp conf/nginx/nginx.conf.webserver $NGINX_PATH/conf/nginx.conf
-}
 
 setup_proxy()
 {
@@ -17,21 +13,13 @@ setup_proxy()
 	sed -i "s/backend_server/$backend_server/g" $NGINX_PATH/conf/nginx.conf
 }
 
-setup_lb()
-{
-	cp conf/nginx/nginx.conf.lb $NGINX_PATH/conf/nginx.conf
-	sed -i "s/backend_server/$backend_server/g" $NGINX_PATH/conf/nginx.conf
-}
-
-setup_lb
+setup_proxy
 
 # start nginx
 $NGINX_PATH/sbin/nginx -s stop
 sleep 1
 $NGINX_PATH/sbin/nginx
-
 sleep 1
-
 ps -ef | grep nginx | grep -v grep | grep -v "start-nginx" | wc -l
 
 # stop nginx
